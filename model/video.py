ca344805e28b7411dfc8a5c43026995e45ce1c76
Wisley Bruno <srmarquesms@gmail.com>
2022-11-28 01:15:41 -0400

    add

--- model/video.py
+++ model/video.py
@@ -1,5 +1,6 @@
 class Video:
-    def __init__(self, author='', title='', duration='', items=[]):
+    def __init__(self, videoid='', author='', title='', duration='', items=[]):
+        self.videoid = videoid
         self.author = author
         self.title = title
         self.duration = duration
@@ -7,7 +8,12 @@ class Video:
 
 
 class Item:
-    def __init__(self, extension='', resolution='', url=''):
+    def __init__(self, key, extension='', resolution='', url='', bitrate='', rawbitrate='', filesize=''):
+        self.key = key
         self.extension = extension
         self.resolution = resolution
         self.url = url
+        self.bitrate = bitrate
+        self.rawbitrate = rawbitrate
+        self.filesize = filesize
+
